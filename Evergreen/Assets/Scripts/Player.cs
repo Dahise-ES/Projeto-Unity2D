a7e2a0c341801773f78d699e01b7bfde27e4d616
DahiseDias <dahisedias@gmail.com>
2021-07-12 10:43:50 -0300

    divisão das pastas

---
+++ Evergreen/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float velocity;
+    enum Direction
+    {
+        Up, Down, Left, Right, Idle
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
